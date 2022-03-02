b698721cc02511c9cc4049619d9a9fc419ec3398
JackLais <jlais@bsd220.org>
2022-03-02 10:17:09 -0600

    created a lose zone to BREAKOUT BREAKDOWN

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,6 +11,8 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
+    var loseZone = SKSpriteNode()
     override func didMove(to view: SKView) {
         createBackground()
         resetGame()
@@ -20,6 +22,8 @@ class GameScene: SKScene {
         // this stuff happens before each game starts
         makeBall()
         makePaddle()
+        makeBrick()
+        makeLoseZone()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -70,4 +74,21 @@ class GameScene: SKScene {
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
     }
+    func makeBrick() {
+           brick.removeFromParent()    // remove the brick, if it exists
+           brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+           brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+           brick.name = "brick"
+           brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+           brick.physicsBody?.isDynamic = false
+           addChild(brick)
+       }
+    func makeLoseZone() {
+            loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
+            loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+            loseZone.name = "loseZone"
+            loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+            loseZone.physicsBody?.isDynamic = false
+            addChild(loseZone)
+        }
 }
